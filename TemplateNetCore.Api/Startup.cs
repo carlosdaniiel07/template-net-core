d1ddf9de408c54ff5888ae8066e995cee627d092
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-07-06 21:22:41 -0300

    refactor: remove unused usings

--- TemplateNetCore.Api/Startup.cs
+++ TemplateNetCore.Api/Startup.cs
@@ -1,8 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using TemplateNetCore.Api.Infraestructure.Extensions;
 using TemplateNetCore.Api.Middlewares;
 using TemplateNetCore.Infra.Mapping;
