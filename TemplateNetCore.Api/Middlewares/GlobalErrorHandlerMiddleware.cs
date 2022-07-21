e85e1b37dcbf088f35449af000c47480f5d8ff9f
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-07-21 20:17:53 -0300

    feat: add ILogger to GlobalErrorHandlerMiddleware

--- TemplateNetCore.Api/Middlewares/GlobalErrorHandlerMiddleware.cs
+++ TemplateNetCore.Api/Middlewares/GlobalErrorHandlerMiddleware.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System.Net;
 using System.Text.Json;
 
 using TemplateNetCore.Service.Exceptions;
@@ -11,10 +8,12 @@ namespace TemplateNetCore.Api.Middlewares
     public class GlobalErrorHandlerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalErrorHandlerMiddleware> _logger;
 
-        public GlobalErrorHandlerMiddleware(RequestDelegate next)
+        public GlobalErrorHandlerMiddleware(RequestDelegate next, ILogger<GlobalErrorHandlerMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext context)
@@ -30,8 +29,9 @@ namespace TemplateNetCore.Api.Middlewares
                 response.StatusCode = ex is CustomException ? (ex as CustomException).StatusCode : (int)HttpStatusCode.InternalServerError;
 
                 var message = response.StatusCode == 500 ? "Ocorreu um erro desconhecido. Tente novamente mais tarde" : ex.Message;
-
                 var jsonResponse = JsonSerializer.Serialize(new { message });
+
+                _logger.LogError(ex, message);
 
                 await response.WriteAsync(jsonResponse);
             }
