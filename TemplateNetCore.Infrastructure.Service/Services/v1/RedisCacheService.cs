315652389747875280fff7be0f43f538f67f5307
Carlos Daniel Martins de Almeida <carlosd.almeida@outlook.com>
2023-04-22 18:37:19 -0300

    feat: add Redis implementation

---
+++ TemplateNetCore.Infrastructure.Service/Services/v1/RedisCacheService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System.Text.Json;
+using TemplateNetCore.Domain.Interfaces.Services.v1;
+
+namespace TemplateNetCore.Infrastructure.Service.Services.v1
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IDatabase _database;
+
+        public RedisCacheService(IConfiguration configuration)
+        {
+            var connection = ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis"));
+            _database = connection.GetDatabase();
+
+        }
+
+        public async Task AddAsync<T>(string key, T value, TimeSpan ttl)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                return;
+
+            var json = JsonSerializer.Serialize(value, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+
+            await _database.StringSetAsync(key, json, ttl);
+        }
+
+        public async Task<T> RetrieveAsync<T>(string key)
+        {
+            var value = await _database.StringGetAsync(key);
+
+            if (!value.HasValue)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(value.ToString(), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            });
+        }
+
+        public async Task DeleteAsync(string key)
+        {
+            await _database.KeyDeleteAsync(key);
+        }
+    }
+}
