b9af77dbd801716cd66c01f3a6845fec0a4c2571
Carlos Daniel Martins de Almeida <hdn.carlos@voeazul.com.br>
2023-08-14 22:06:58 -0300

    refactor: enable Swagger only in development environment

--- TemplateNetCore.Api/Program.cs
+++ TemplateNetCore.Api/Program.cs
@@ -9,6 +9,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Template .NET Core");
+    });
     app.UseDeveloperExceptionPage();
 }
 
@@ -17,11 +22,6 @@ app.UseRouting();
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Template .NET Core");
-});
 app.UseMiddleware<GlobalErrorHandlerMiddleware>();
 app.UseEndpoints(endpoints =>
 {
