d95babc71bf580a86043a119f43f0d573ec65e73
Carlos Daniel Martins de Almeida <hdn.carlos@voeazul.com.br>
2024-01-09 22:43:07 -0300

    refactor: general improvements in RedisCacheService class

--- TemplateNetCore.Domain/Interfaces/Services/v1/ICacheService.cs
+++ TemplateNetCore.Domain/Interfaces/Services/v1/ICacheService.cs
@@ -3,7 +3,7 @@
     public interface ICacheService
     {
         Task<T> RetrieveAsync<T>(string key);
-        Task AddAsync<T>(string key, T value, TimeSpan ttl);
+        Task AddAsync<T>(string key, T value, TimeSpan? ttl);
         Task DeleteAsync(string key);
     }
 }
--- TemplateNetCore.Infrastructure.Service/Services/v1/RedisCacheService.cs
+++ TemplateNetCore.Infrastructure.Service/Services/v1/RedisCacheService.cs
@@ -8,47 +8,44 @@ namespace TemplateNetCore.Infrastructure.Service.Services.v1
     public class RedisCacheService : ICacheService
     {
         private readonly IDatabase _database;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
 
         public RedisCacheService(IConfiguration configuration)
         {
             var connection = ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis"));
+
             _database = connection.GetDatabase();
-        }
-
-        public async Task AddAsync<T>(string key, T value, TimeSpan ttl)
-        {
-            if (string.IsNullOrWhiteSpace(key))
-                throw new ArgumentNullException(nameof(key));
-
-            var json = JsonSerializer.Serialize(value, new JsonSerializerOptions
+            _jsonSerializerOptions = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            });
+                PropertyNameCaseInsensitive = true,
+            };
+        }
+
+        public async Task AddAsync<T>(string key, T value, TimeSpan? ttl)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
+
+            var json = JsonSerializer.Serialize(value, _jsonSerializerOptions);
 
             await _database.StringSetAsync(key, json, ttl);
         }
 
         public async Task<T> RetrieveAsync<T>(string key)
         {
-            if (string.IsNullOrWhiteSpace(key))
-                throw new ArgumentNullException(nameof(key));
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
 
             var value = await _database.StringGetAsync(key);
 
             if (!value.HasValue)
                 return default;
 
-            return JsonSerializer.Deserialize<T>(value.ToString(), new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-            });
+            return JsonSerializer.Deserialize<T>(value.ToString(), _jsonSerializerOptions);
         }
 
         public async Task DeleteAsync(string key)
         {
-            if (string.IsNullOrWhiteSpace(key))
-                throw new ArgumentNullException(nameof(key));
-
+            ArgumentException.ThrowIfNullOrWhiteSpace(key);
             await _database.KeyDeleteAsync(key);
         }
     }
